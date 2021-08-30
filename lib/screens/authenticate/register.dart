abf3373486950cf8c6ee6af7e75570aaa9e99ffa
sherange <sherangef@gmail.com>
2021-08-30 18:23:26 +0530

    flutter spinkit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:brew_crew/services/auth_service.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -17,77 +18,83 @@ class _RegisterState extends State<Register> {
   String email = "";
   String password = "";
   String error = "";
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Register to Brew"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.login_rounded),
-          ),
-        ],
-      ),
-      backgroundColor: Colors.brown[100],
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textDecoration.copyWith(hintText: "Email"),
-                validator: (val) => val!.isEmpty ? "Enter an email" : null,
-                onChanged: (val) => {setState(() => email = val)},
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textDecoration.copyWith(hintText: "Password"),
-                obscureText: true,
-                validator: (val) =>
-                    val!.length < 6 ? "Enter password 6+ chars long" : null,
-                onChanged: (val) => {setState(() => password = val)},
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 50)),
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.brown[400])),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic newUser = await _auth
-                          .registerWithEmailAndPassword(email, password);
-
-                      if (newUser == null) {
-                        setState(() {
-                          error = "Registration failed";
-                        });
-                      }
-                      print(newUser);
-                    }
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text("Register to Brew"),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  child: Text(
-                    "Register",
-                    style: TextStyle(color: Colors.white),
-                  )),
-              SizedBox(height: 20.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                  icon: Icon(Icons.login_rounded),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.brown[100],
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textDecoration.copyWith(hintText: "Email"),
+                      validator: (val) =>
+                          val!.isEmpty ? "Enter an email" : null,
+                      onChanged: (val) => {setState(() => email = val)},
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textDecoration.copyWith(hintText: "Password"),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? "Enter password 6+ chars long"
+                          : null,
+                      onChanged: (val) => {setState(() => password = val)},
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                                EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 50)),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.brown[400])),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic newUser = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (newUser == null) {
+                              setState(() {
+                                loading = false;
+                                error = "Registration failed";
+                              });
+                            }
+                          }
+                        },
+                        child: Text(
+                          "Register",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    SizedBox(height: 20.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:brew_crew/services/auth_service.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -17,75 +18,83 @@ class _SignInState extends State<SignIn> {
   String email = "";
   String password = "";
   String error = "";
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign in to Brew"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.app_registration_rounded),
-          ),
-        ],
-      ),
-      backgroundColor: Colors.brown[100],
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textDecoration.copyWith(hintText: "Email"),
-                validator: (val) => val!.isEmpty ? "Enter an email" : null,
-                onChanged: (val) => {setState(() => email = val)},
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textDecoration.copyWith(hintText: "Password"),
-                obscureText: true,
-                validator: (val) => val!.isEmpty ? "Enter password" : null,
-                onChanged: (val) => {setState(() => password = val)},
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 50)),
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.brown[400])),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic user = await _auth.signInWithEmailAndPassword(
-                          email, password);
-
-                      if (user == null) {
-                        setState(() {
-                          error = "SignIn failed";
-                        });
-                      }
-                    }
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text("Sign in to Brew"),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(color: Colors.white),
-                  )),
-              SizedBox(height: 20.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                  icon: Icon(Icons.app_registration_rounded),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.brown[100],
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textDecoration.copyWith(hintText: "Email"),
+                      validator: (val) =>
+                          val!.isEmpty ? "Enter an email" : null,
+                      onChanged: (val) => {setState(() => email = val)},
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textDecoration.copyWith(hintText: "Password"),
+                      obscureText: true,
+                      validator: (val) =>
+                          val!.isEmpty ? "Enter password" : null,
+                      onChanged: (val) => {setState(() => password = val)},
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                                EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 50)),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.brown[400])),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic user = await _auth
+                                .signInWithEmailAndPassword(email, password);
+
+                            if (user == null) {
+                              setState(() {
+                                loading = false;
+                                error = "SignIn failed";
+                              });
+                            }
+                          }
+                        },
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    SizedBox(height: 20.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
